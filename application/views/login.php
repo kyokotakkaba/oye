feeae4ae96a83d1cd88a453a2153cbfcd482ab7a
Yoko <yoko.developer@gmail.com>
2019-07-02 11:30:03 +0700

    view (login & dashboard UI)
    apply AdminLTE template
    - login UI n ajax
    - dashboard UI

--- application/views/login.php
+++ application/views/login.php
@@ -1,14 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Login</title>
+<title>Login Oye</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- Tell the browser to be responsive to screen width -->
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<!-- Bootstrap 3.3.7 -->
+<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+
+<!-- Google Font -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body>
-	<form action="<?php echo base_url(); ?>index.php/ceklogin" method="POST">
-	username: <input type="text" name="username"> <br>
-	password: <input type="password" name="password"> <br>
-	<input type="submit" value="submit">
-	</form>
+
+<body class="hold-transition login-page">
+<div class="login-box">
+<div class="login-logo">
+<a href="../../index2.html"><b>Welcome</b>OYE</a>
+</div>
+<!-- /.login-logo -->
+<div class="login-box-body">
+<p class="login-box-msg">Sign in</p>
+
+<form action="<?php echo base_url(); ?>index.php/ceklogin" method="POST">
+<div class="form-group has-feedback">
+<input type="text" class="form-control" name="username" placeholder="Username">
+<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+</div>
+<div class="form-group has-feedback">
+<input type="password" class="form-control" name="password" placeholder="Password">
+<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+</div>
+<div class="row">
+
+<!-- /.col -->
+<div class="col-xs-4">
+<button type="submit" id="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+</div>
+<!-- /.col -->
+</div>
+</form>
+</div>
+<!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Main JS -->
+<script>
+(function (document, window, $) {
+    'use strict';
+    var Site = window.Site;
+    $(document).ready(function () {
+        Site.run();
+    });
+})(document, window, jQuery);
+$(document).on('click', '#submit', function(){
+    var urls='ceklogin';
+    var dataString = $("#form").serialize();
+    $("#submit").html("<?php echo $tunggu ?>");
+    $("#submit").prop("disabled",true);
+    $.ajax({
+        url:"<?php echo base_url() ?>index.php/"+urls,
+        type: 'POST',
+        data:dataString,
+        success: function(response){
+            if (response == member) {
+                window.location.href = "<?php echo base_url() ?>index.php/dashboardmember";
+            }else if(response == admin){
+                window.location.href = "<?php echo base_url() ?>index.php/dashboardmember";
+            }else{
+                alert(response);
+                $("#submit").prop("disabled",false);
+            }
+        }
+    });
+});
+</script>
 </body>
 </html>
