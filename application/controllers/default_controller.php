10a3949aef5ff3526855819f92712ff312a0d8a3
Enny Hartz Tono <djaka.tingkir2@gmail.com>
2019-07-03 17:48:45 +0700

    rapikan susunan dan tambah file view dan routing

--- application/controllers/default_controller.php
+++ application/controllers/default_controller.php
@@ -5,98 +5,17 @@ class default_controller extends CI_Controller {
 		$this->load->model('default_model');
 		$this->load->helper('url_helper');
 	}
+	//LOAD VIEW
 
+	//front
 	public function index(){
 		$this->load->view('frontpage');
 	}
 
-
-	//Login
+	//login
 	public function login(){
 		$this->load->view('login');
 	}
-
-	public function ceklogin(){
-		$username = $this->input->post('username');
-		$password = md5($this->input->post('password'));
-		$data = $this->default_model->get_data_admin();
-		if ($username == $data['username'] && $password == $data['password']) {
-			$this->load->helper('cookie');
-			$cookie= array(
-				'name'   => 'backendCookie',
-				'value'  => $username,
-				'expire' => '0',
-			);
-			$this->input->set_cookie($cookie);
-			echo "berhasil login admin";
-		}else{
-			$data = $this->default_model->get_data_member(NULL);
-			$is_login = false;
-			foreach ($data as $row){
-				if ($username == $row['username'] && $password == $row['password']) {
-					$this->load->helper('cookie');
-					$cookie= array(
-						'name'   => 'memberCookie',
-						'value'  => $username,
-						'expire' => '0',
-					);
-					$this->input->set_cookie($cookie);
-					$is_login = true;
-					break;
-				}
-			}
-
-			if ($is_login) {
-				echo "berhasil login member";
-			}else{
-				echo "gagal login";
-			}
-		}
-
-	}
-
-	//Check cookie
-	public function checkcookieadmin(){
-		$this->load->helper('cookie');
-		if ($this->input->cookie('backendCookie',true)!=NULL) {
-			// echo $this->input->cookie('backendCookie'); //to output cookie
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-
-	public function checkcookiemember(){
-		$this->load->helper('cookie');
-		if ($this->input->cookie('memberCookie',true)!=NULL) {
-			// echo $this->input->cookie('memberCookie'); //to output cookie
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-
-	//Logout
-	public function logoutadmin(){
-		$this->load->helper('cookie');
-		delete_cookie("memberCookie");
-		header("Location: ".base_url()."index.php/login");
-		die();
-	}
-
-	public function logoutmember(){
-		$this->load->helper('cookie');
-		delete_cookie("memberCookie");
-		header("Location: ".base_url()."index.php/login");
-		die();
-	}
-
-
-
-
-
 
 	//Dashboard
 	public function dashboardadmin(){
@@ -117,40 +36,40 @@ class default_controller extends CI_Controller {
 		}
 	}
 
-
-
-	//Ubah password
+	//change password
 	public function changepasswordadmin(){
-		$oldpassword = md5($this->input->post('oldpassword'));
-		$data = $this->default_model->get_data_admin();
-		if ($oldpassword == $data['password']){
-			$data = array(
-				'password' => md5($this->input->post('newpassword'))
-			);
-			$insertStatus = $this->default_model->update_password_admin($this->input->cookie('backendCookie',true),$data);
-			echo $insertStatus;
+		if ($this->checkcookieadmin()) {
+			$this->load->view('admin/change_password');
 		}else{
-			echo "password lama salah";
+			header("Location: ".base_url()."index.php/login");
+			die();
 		}
 	}
 
 	public function changepasswordmember(){
-		$oldpassword = md5($this->input->post('oldpassword'));
-		$data = $this->default_model->get_data_admin();
-		if ($oldpassword == $data['password']){
-			$data = array(
-				'password' => md5($this->input->post('newpassword'))
-			);
-			$insertStatus = $this->default_model->update_password_member($this->input->cookie('memberCookie',true),$data);
-			echo $insertStatus;
+		if ($this->checkcookiemember()) {
+			$this->load->view('member/change_password');
 		}else{
-			echo "password lama salah";
+			header("Location: ".base_url()."index.php/login");
+			die();
+		}
+	}
+
+	//profile
+	public function editprofile(){
+		if ($this->checkcookiemember()) {
+			$this->load->view('member/edit_profile');
+		}else{
+			header("Location: ".base_url()."index.php/login");
+			die();
 		}
 	}
 
 
 
 
+
+	//GET DATA
 
 	//Data member
 	public function get_currentuser(){
@@ -252,6 +171,124 @@ class default_controller extends CI_Controller {
 		// 	}
 		// }
 		echo json_encode($data);
+	}
+
+
+
+
+	//UPDATE
+
+	//Ubah password
+	public function updatepasswordadmin(){
+		$oldpassword = md5($this->input->post('oldpassword'));
+		$data = $this->default_model->get_data_admin();
+		if ($oldpassword == $data['password']){
+			$data = array(
+				'password' => md5($this->input->post('newpassword'))
+			);
+			$insertStatus = $this->default_model->update_password_admin($this->input->cookie('backendCookie',true),$data);
+			echo $insertStatus;
+		}else{
+			echo "password lama salah";
+		}
+	}
+
+	public function updatepasswordmember(){
+		$oldpassword = md5($this->input->post('oldpassword'));
+		$data = $this->default_model->get_data_admin();
+		if ($oldpassword == $data['password']){
+			$data = array(
+				'password' => md5($this->input->post('newpassword'))
+			);
+			$insertStatus = $this->default_model->update_password_member($this->input->cookie('memberCookie',true),$data);
+			echo $insertStatus;
+		}else{
+			echo "password lama salah";
+		}
+	}
+
+
+
+
+
+	//OTHER
+
+	//Login
+	public function ceklogin(){
+		$username = $this->input->post('username');
+		$password = md5($this->input->post('password'));
+		$data = $this->default_model->get_data_admin();
+		if ($username == $data['username'] && $password == $data['password']) {
+			$this->load->helper('cookie');
+			$cookie= array(
+				'name'   => 'backendCookie',
+				'value'  => $username,
+				'expire' => '0',
+			);
+			$this->input->set_cookie($cookie);
+			echo "berhasil login admin";
+		}else{
+			$data = $this->default_model->get_data_member(NULL);
+			$is_login = false;
+			foreach ($data as $row){
+				if ($username == $row['username'] && $password == $row['password']) {
+					$this->load->helper('cookie');
+					$cookie= array(
+						'name'   => 'memberCookie',
+						'value'  => $username,
+						'expire' => '0',
+					);
+					$this->input->set_cookie($cookie);
+					$is_login = true;
+					break;
+				}
+			}
+
+			if ($is_login) {
+				echo "berhasil login member";
+			}else{
+				echo "gagal login";
+			}
+		}
+
+	}
+
+	//Check cookie
+	public function checkcookieadmin(){
+		$this->load->helper('cookie');
+		if ($this->input->cookie('backendCookie',true)!=NULL) {
+			// echo $this->input->cookie('backendCookie'); //to output cookie
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	public function checkcookiemember(){
+		$this->load->helper('cookie');
+		if ($this->input->cookie('memberCookie',true)!=NULL) {
+			// echo $this->input->cookie('memberCookie'); //to output cookie
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	//Logout
+	public function logoutadmin(){
+		$this->load->helper('cookie');
+		delete_cookie("memberCookie");
+		header("Location: ".base_url()."index.php/login");
+		die();
+	}
+
+	public function logoutmember(){
+		$this->load->helper('cookie');
+		delete_cookie("memberCookie");
+		header("Location: ".base_url()."index.php/login");
+		die();
 	}
 
 	
