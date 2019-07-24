810c8f4ae957f2ee606127d9857248edf43a102b
Enny Hartz Tono <djaka.tingkir2@gmail.com>
2019-07-24 20:03:35 +0700

    remove file to refresh filename

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'default_controller';
-$route['(:any)'] = 'default_controller/$1';
-$route['(:any)/(.+)'] = 'default_controller/$1/$2';
+$route['default_controller'] = 'Default_controller';
+$route['(:any)'] = 'Default_controller/$1';
+$route['(:any)/(.+)'] = 'Default_controller/$1/$2';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
